520fead2eee3ddd2319776da183bd441922fb5d4
Aadit <aaditbhojgi@gmail.com>
2017-06-13 23:05:54 +0530

    Indentation fixed and Comments added

--- media.py
+++ media.py
@@ -1,11 +1,18 @@
+# This module allows us to open the desired Web Page.
 import webbrowser
 
-class Movie():
-    """ This my class """
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+
+# This Class is called and assigned values in ent_.py
+# and then passed as a list to the Html Page
+class Movie:
+    """ This is the main Class """
+
+    def __init__(self, movie_title, movie_storyline, poster_image,
+                 trailer_youtube):
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
+
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
